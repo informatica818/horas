e6bcb8fbeee85edf5b9e7ccd1d21a03fb9536010
chris2555 <christianhernandez3050@gmail.com>
2024-12-16 07:24:34 -0600

    registrer agregado

--- resources/views/login/recuperar-cuenta.blade.php
+++ resources/views/login/recuperar-cuenta.blade.php
@@ -1,25 +1,56 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Password Reset</title>
+</head>
+<body class="h-100">
+    <section class="h-100">
+        <div class="container h-100">
+            <div class="row justify-content-sm-center h-100">
+                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+                    <div class="text-center my-5">
+                        <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100">
+                    </div>
+                    <div class="card shadow-lg">
+                        <div class="card-body p-5">
+                            <p class="mb-4 text-muted text-sm">
+                                Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+                            </p>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <!-- Session Status -->
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+                            <form method="POST" action="{{ route('password.email') }}" class="needs-validation" novalidate="">
+                                @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                <!-- Email Address -->
+                                <div class="mb-3">
+                                    <label for="email" class="form-label text-muted">Email</label>
+                                    <input id="email" type="email" class="form-control" name="email" :value="old('email')" required autofocus>
+                                    <div class="invalid-feedback">
+                                        Please enter a valid email address.
+                                    </div>
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                </div>
+
+                                <div class="d-flex align-items-center justify-content-end mt-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Email Password Reset Link
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="text-center mt-5 text-muted">
+                            Copyright &copy; 2017-2021 &mdash; Your Company
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+</body>
+</html>
--- resources/views/login/registro.blade.php
+++ resources/views/login/registro.blade.php
@@ -1,53 +1,84 @@
-<x-guest-layout>
-    
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </head>
+
+    <section class="h-100">
+        <div class="container h-100">
+            <div class="row justify-content-sm-center h-100">
+                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+                    <div class="text-center my-5">
+                        <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100">
+                    </div>
+                    <div class="card shadow-lg">
+                        <div class="card-body p-5">
+                            <h1 class="fs-4 card-title fw-bold mb-4">Registro</h1>
+                            <form method="POST" action="{{ route('register') }}" class="needs-validation" novalidate>
+                                @csrf
+
+                                <!-- Nombre -->
+                                <div class="mb-3">
+                                    <label for="name" class="mb-2 text-muted">Nombre</label>
+                                    <input id="name" type="text" class="form-control" name="name" :value="old('name')" required autofocus autocomplete="name">
+                                    <div class="invalid-feedback">
+                                        El nombre es obligatorio
+                                    </div>
+                                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                </div>
+
+                                <!-- Correo electrónico -->
+                                <div class="mb-3">
+                                    <label for="email" class="mb-2 text-muted">Correo electrónico</label>
+                                    <input id="email" type="email" class="form-control" name="email" :value="old('email')" required autocomplete="username">
+                                    <div class="invalid-feedback">
+                                        El correo electrónico no es válido
+                                    </div>
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                </div>
+
+                                <!-- Contraseña -->
+                                <div class="mb-3">
+                                    <label for="password" class="mb-2 text-muted">Contraseña</label>
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                                    <div class="invalid-feedback">
+                                        La contraseña es obligatoria
+                                    </div>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                </div>
+
+                                <!-- Confirmar contraseña -->
+                                <div class="mb-3">
+                                    <label for="password_confirmation" class="mb-2 text-muted">Confirmar contraseña</label>
+                                    <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    <div class="invalid-feedback">
+                                        La confirmación de contraseña es obligatoria
+                                    </div>
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                </div>
+
+                                <div class="d-flex align-items-center">
+                                    <a href="{{ route('login') }}" class="text-muted me-auto">
+                                        ¿Ya tienes una cuenta?
+                                    </a>
+                                    <button type="submit" class="btn btn-success ms-auto">
+                                        Registrarse
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer py-1 border-0">
+                            <div class="text-center">
+                                ¿Ya tienes una cuenta? <a href="{{ route('login') }}" class="text-success">Inicia sesión</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center mt-5 text-muted">
+                        Copyright &copy; 2017-2021 &mdash; Tu Empresa
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
